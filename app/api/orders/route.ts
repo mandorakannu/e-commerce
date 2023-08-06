df94a2b75404d130ecdc8068fa1f8d277ec0fe03
mandorakannu <mandorakannu@gmail.com>
2023-08-06 17:29:44 +0530

    now all orders can save inside the mongodb

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -19,21 +19,34 @@ export async function POST(request: NextRequest, response: NextResponse) {
       cart,
     } = await request.json();
 
-    const order = await Orders.create({
-      firstName,
-      lastName,
-      email,
-      address,
-      address2,
-      city,
-      region,
-      country,
-      zipcode,
-      phone,
-      cart,
-    });
-    disconnectDB();
-    return NextResponse.json(order, { status: 201 });
+    const isUserOrderAvaiable = await Orders.findOne({ email });
+    if (isUserOrderAvaiable) {
+      const order = await Orders.findOneAndUpdate(
+        { email },
+        {
+          // update the current cart with the new cart
+          $set: { cart: isUserOrderAvaiable.cart.concat(cart) },
+        }
+      );
+      disconnectDB();
+      return NextResponse.json(order, { status: 200 });
+    } else {
+      const order = await Orders.create({
+        firstName,
+        lastName,
+        email,
+        address,
+        address2,
+        city,
+        region,
+        country,
+        zipcode,
+        phone,
+        cart,
+      });
+      disconnectDB();
+      return NextResponse.json(order, { status: 201 });
+    }
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
